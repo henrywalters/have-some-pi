f7a7b18aecbd3acdba66a0dc33f5ffb853ea476d
Henry Walters <henrywalters20@gmail.com>
2020-12-24 02:45:14 -0500

    Extracted LCD to class

--- LCD.cpp
+++ LCD.cpp
@@ -18,69 +18,91 @@ int pcf8754_address =0x27;
 #define D6 BASE + 6
 #define D7 BASE + 7
 
-int lcdhd;
+class LCD {
+    int _lcd;
+    int _address;
 
-int detectI2C(int addr) {
-    int fd = wiringPiI2CSetup(addr);
-    if (fd < 0) {
-        printf("ERROR ADDRESS :0x%x \n", addr);
-        return 0;
-    } else {
-        if (wiringPiI2CWrite(fd, 0) < 0) {
-            printf("Not found device at address 0x%x \n", addr);
+    int detectI2C(int addr) {
+        int fd = wiringPiI2CSetup(addr);
+        if (fd < 0) {
+            printf("ERROR ADDRESS :0x%x \n", addr);
             return 0;
         } else {
-            printf("Found device at 0x%x \n", addr);
-            return 1;
+            if (wiringPiI2CWrite(fd, 0) < 0) {
+                printf("Not found device at address 0x%x \n", addr);
+                return 0;
+            } else {
+                printf("Found device at 0x%x \n", addr);
+                return 1;
+            }
         }
     }
-}
 
-void printCpuTemp() {
+public:
+    LCD() {
+        if (detectI2C(0x27)) {
+            _address = 0x27;
+        } else if (detectI2C(0x3F)) {
+            _address = 0x3F;
+        } else {
+            printf("Failedf to find address. run i2cdetect -y l");
+        }
+
+        pcf8574Setup(BASE, pcf8754_address);
+
+        digitalWrite(LED, HIGH);
+        digitalWrite(RW, LOW);
+        _lcd = lcdInit(2, 16, 4, RS, EN, D4, D5, D6, D7, 0, 0, 0, 0);
+    }
+
+    void setCursorPos(int x, int y) {
+        lcdPosition(_lcd, x, y);
+    }
+
+    void print(char* str) {
+        lcdPrintf(_lcd, str);
+    }
+
+};
+
+char* printCpuTemp() {
     FILE *fp;
+    char* out;
     char str_temp[15];
     float temp;
 
     fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
     fgets(str_temp, 15, fp);
     temp = atof(str_temp) / 1000.0;
-
-    lcdPosition(lcdhd, 0, 0);
-    lcdPrintf(lcdhd, "CPU: %.2fC", temp);
     fclose(fp);
+
+    sprintf(out, "CPU: %.2fC", temp);
+
+    return out;
 }
 
-void printDateTime() {
+char* printDateTime() {
     time_t raw;
+    char* out;
     struct tm* timeinfo;
     time(&raw);
     timeinfo = localtime(&raw);
-    printf("%s \n", asctime(timeinfo));
-    lcdPosition(lcdhd, 0, 1);
-
-    lcdPrintf(lcdhd, "Time: %02d:%02d:%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+    sprintf(out, "Time: %02d:%02d:%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+    return out;
 }
 
 int main(void) {
     wiringPiSetup();
 
-    if (detectI2C(0x27)) {
-        pcf8754_address = 0x27;
-    } else if (detectI2C(0x3F)) {
-        pcf8754_address = 0x3F;
-    } else {
-        printf("Failedf to find address. run i2cdetect -y l");
-    }
-
-    pcf8574Setup(BASE, pcf8754_address);
-
-    digitalWrite(LED, HIGH);
-    digitalWrite(RW, LOW);
-    lcdhd = lcdInit(2, 16, 4, RS, EN, D4, D5, D6, D7, 0, 0, 0, 0);
+    LCD lcd;
 
     while (1) {
-        printCpuTemp();
-        printDateTime();
+        char* temp = printCpuTemp();
+        char* time = printDateTime();
+        lcd.setCursorPos(0, 0);
+        lcd.print(temp);
+        lcd.setCursorPos(0, 1);
+        lcd.print(time);
         delay(1000);
     }
 
