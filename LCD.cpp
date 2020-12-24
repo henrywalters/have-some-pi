ac463588598d724aac3519d15dd3fcb6be4ea2b8
Henry Walters <henrywalters20@gmail.com>
2020-12-24 02:46:57 -0500

    Initialize check

--- LCD.cpp
+++ LCD.cpp
@@ -50,6 +50,10 @@ public:
 
         pcf8574Setup(BASE, _address);
 
+        if (_lcd == -1) {
+            printf("Failed to initialize lcd");
+        }
+
         digitalWrite(LED, HIGH);
         digitalWrite(RW, LOW);
         _lcd = lcdInit(2, 16, 4, RS, EN, D4, D5, D6, D7, 0, 0, 0, 0);
