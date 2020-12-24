416cd7f1dc94828561bf73db4f3258e0080a3728
Henry Walters <henrywalters20@gmail.com>
2020-12-23 19:54:44 -0500

    add some flare

--- Blink.cpp
+++ Blink.cpp
@@ -3,18 +3,37 @@
 
 #define ledPin 0
 
+int fibb(int n) {
+    if (n == 1) {
+        return 1
+    } else if (n == 2) {
+        return 1
+    } else {
+        return fibb[n - 1] + fibb[n - 2];
+    }
+}
+
 int main(void) {
     printf("Starting program... \n");
+
+    int fibbs[20] = {}
+
+    for (int i = 1; i < 20; i++) {
+        fibbs[i - 1] = fibb(i);
+    }
 
     wiringPiSetup();
 
     pinMode(ledPin, OUTPUT);
 
+    int fIndex = 0;
+
     while(1) {
         digitalWrite(ledPin, HIGH);
-        delay(1000);
+        delay(fibbs[fIndex % 20]);
         digitalWrite(ledPin, LOW);
-        delay(1000);
+        delay(fibbs[fIndex % 20]);
+        fIndex++;
     }
 
     return 0;
